4f1f784f30e840e6d1d1114945916fc1a2f242f0
Emmanuel Alvarado <emmanuel_alvap@hotmail.com>
2026-03-13 00:01:14 -0600

    init

--- backend/debt.py
+++ backend/debt.py
@@ -3,12 +3,25 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
-def sarimax():   
+def sci_to_text(num):
+    #Converts a number in scientific notation to a plain string without exponent."
+    #Handles integers, floats, and strings.
+    try:
+        num_float = float(num)
+        temp = format(num_float, 'f').rstrip('0').rstrip('.') if '.' in format(num_float, 'f') else format(num_float, 'f')
+        temp = temp.split('.', 1)[0]
+        num = int(temp)
+        num = f"{num:,}"
+        return num
+    except (ValueError, TypeError):
+        raise ValueError("Input must be a number or numeric string.")
+
+def sarimax(df):   
     
-    current_dir = Path(__file__).resolve().parent
-    parent_dir = current_dir.parent
-    file_path = parent_dir / "data//mexico.csv"
-    df = pd.read_csv(file_path)
+    #current_dir = Path(__file__).resolve().parent
+    #parent_dir = current_dir.parent
+    #file_path = parent_dir / "data//mexico.csv"
+    #df = pd.read_csv(file_path)
     df.set_index("year", inplace=True)
     
     # Variable objetivo
@@ -26,4 +39,57 @@ def sarimax():
     print("-------------------------------------------------------------------------------------------------------------------------------")
     print("-------------------------------------------------------------------------------------------------------------------------------")
 
-sarimax()
+
+    future_years = list(range(2027, 2041))
+    #print("tamano")
+    #print(len(future_years) )
+    future_interes = []
+    for i in range (0, 14):
+        future_interes.append(6.6)
+    
+    future_pib = []
+    for i in range(0, 14):
+        future_pib.append(1.89e12)
+    
+    future_tipoc = []
+    for i in range(0, 14):
+        future_tipoc.append(22.4)
+    
+    future_exog = pd.DataFrame({
+        "pib": [2.0e12 + i*5e10 for i in range(len(future_years))],   # hypothetical GDP growth,
+        "interes": [5 + 0.1*i for i in range(len(future_years))],     # hypothetical interest rates,
+        "tipoc": [18 + 0.05*i for i in range(len(future_years))]      # hypothetical exchange rate,
+    }, index=future_years)
+
+    #print(future_exog)
+
+    future_exog2 = pd.DataFrame({
+        "pib": future_pib,   # hipotetico,
+        "interes": future_interes,     # hipotetico,
+        "tipoc": future_tipoc      # hipotetico,
+    }, index=future_years)
+
+    #print(future_exog2)
+    
+    forecast = results.get_forecast(steps=len(future_years), exog=future_exog)
+    forecast_mean = forecast.predicted_mean
+    forecast_ci = forecast.conf_int()
+    forecast_valor = forecast_mean.apply(sci_to_text)
+    predicciones = pd.DataFrame({
+        "year": future_years,
+        "value": forecast_valor,
+    }),
+    #print("Predicciones del modelo")
+    #print(predicciones)
+    print("-----------------------------------------------------------------")
+    #for item in predicciones:
+        #print(type(item.year))
+        #print(" - ")
+        #print(type(item.value))
+    #print(type(predicciones))
+
+    #columns = ["year", "value"]
+    #result = pd.DataFrame(predicciones, columns=columns)
+    return predicciones
+
+#sarimax()
--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,8 @@ import pandas as pd
 from pathlib import Path
 import numpy as np
 from fastapi.responses import JSONResponse
+import time
+from backend import debt
 
 
 app = FastAPI()
@@ -18,28 +20,115 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class DebtItem(BaseModel):
+debt = "data//deuda.csv"
+pib = "data//pib.csv"
+interes = "data//interes.csv"
+tipoc = "data//tipoc.csv"
+
+class DataItem(BaseModel):
     year: str
     value: str
 
+class Data2(BaseModel):
+    year: str
+    deuda: str
+    pib: str
 
-@app.get("/api/debt/predictions/{country}", response_model=List[DebtItem])
+
+@app.get("/api/debt/predictions/{country}", response_model=List[DataItem])
 def get_data(country):
+
+    start_time = time.perf_counter()
 
     current_dir = Path(__file__).resolve().parent
     parent_dir = current_dir.parent
-    file_path = parent_dir / "data//deuda.csv"
-    df = pd.read_csv(file_path)
+    debt_path = parent_dir / debt
+    pib_path = parent_dir / pib
+    interes_path = parent_dir / interes
+    tipoc_path = parent_dir / tipoc
 
-    countryFilter = df[df['Country Code'] == country]
-    selected_cols = countryFilter.iloc[:, 23:68] 
-    itemsList = []
 
-    for col_name, col_data in selected_cols.items():
-        data = col_data.to_list()[0]
-        itemsList.append(DebtItem(year=col_name, value=data))
+    dfDebt = pd.read_csv(debt_path)
+    dfPib = pd.read_csv(pib_path)
+    dfInteres = pd.read_csv(interes_path)
+    dfTipoc = pd.read_csv(tipoc_path)
 
-#get_data('MEX');
+    countryFilter = dfDebt[dfDebt['Country Code'] == country]
+    selected_cols_debt = countryFilter.iloc[:, 23:68]
+
+    countryFilter = dfPib[dfPib['Country Code'] == country]
+    selected_cols_pib = countryFilter.iloc[:, 23:68]
+
+    countryFilter = dfInteres[dfInteres['Country Code'] == country]
+    selected_cols_interes = countryFilter.iloc[:, 23:68]
+
+    countryFilter = dfTipoc[dfTipoc['Country Code'] == country]
+    selected_cols_tipoc = countryFilter.iloc[:, 23:68]
+
+
+    #df_transposedDebt = selected_cols_debt.T  # or df.transpose()
+    #df_transposedDebt.columns = ['year', 'debt']
+
+    #print(df_transposedDebt.info())
+    #result = pd.merge(selected_cols_debt, selected_cols_pib)
+
+    #print(result.info())
+
+
+
+
+
+    df_all = pd.concat([selected_cols_debt, selected_cols_pib, selected_cols_interes, selected_cols_tipoc], ignore_index=True)
+    #print(df_all.head(2))
+
+    df_transposedDebt = df_all.T 
+    df_transposedDebt.columns = ['deuda', 'pib', 'interes', 'tipoc']
+    df_transposedDebt.insert(0, 'year', range(1980, len(df_transposedDebt) + 1980))
+    #print(df_transposedDebt.columns)
+    
+    
 
     
-    return itemsList
+
+    deuda_medio = df_transposedDebt['deuda'].mean()
+    df_transposedDebt['deuda'] = df_transposedDebt['deuda'].fillna(deuda_medio) 
+    pib_medio = df_transposedDebt['pib'].mean()
+    df_transposedDebt['pib'] = df_transposedDebt['pib'].fillna(pib_medio)
+    interes_medio = df_transposedDebt['interes'].mean()
+    df_transposedDebt['interes'] = df_transposedDebt['interes'].fillna(interes_medio)
+    tipoc_medio = df_transposedDebt['tipoc'].mean()
+    df_transposedDebt['tipoc'] = df_transposedDebt['tipoc'].fillna(tipoc_medio)
+
+
+    #print(df_transposedDebt.head(30))
+
+     #df_filled = df.fillna(0)   
+    predicciones = sarimax(df_transposedDebt);
+
+
+    debtItemsList = []
+
+    #for col_name, col_data in result.items():
+        #data = col_data.to_list()[0]
+        #debtItemsList.append(DataItem(year=col_name, value=data))
+
+
+    for prediccion in predicciones:
+        debtItemsList.append(DataItem(year=prediccion.year.to_string(index=False), value=prediccion.value.to_string(index=False)))      
+
+    #print(type(debtItemsList))
+
+    #for item in debtItemsList:
+        #print(item)
+        #print(" - ")
+        #print(item.value)
+
+    end_time = time.perf_counter()
+
+    elapsed = end_time - start_time
+    print(f"Elapsed time: {elapsed:.6f} seconds")
+
+
+#get_data('MEX');
+    
+    return debtItemsList
